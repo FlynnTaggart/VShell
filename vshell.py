60fa4e487f502a904f55b4b514ef76b950c3a680
Mikhail <mr1302@ya.ru>
2021-09-08 04:50:31 +0300

    Complete rework
    Now zip file is being unpacked and only then vshell works with it. After
    the program stops, a temporary folder with the unpacked archive is being
    removed. Added 'cd' command support. 'ls' and 'pwd' were also improved.

--- vshell.py
+++ vshell.py
@@ -1,27 +1,99 @@
 import zipfile
 import sys
+import signal
+import os
+import shutil
+import platform
+from datetime import datetime, timezone
 from termcolor import colored
+
+user_os = platform.system()
 
 input_path = sys.argv[1]
 
 archive = zipfile.ZipFile(input_path)
-path = zipfile.Path(archive, at='')
+vshell_root_dir = os.getcwd()
+if user_os == 'Windows':
+    vshell_root_dir = vshell_root_dir.replace('\\', '/')
 
-def make_date_string(date_tuple):
-    date_string = ('{:4}'.format(date_tuple[0]) + ' '
-                            + '{:2}'.format(date_tuple[1]) + ' '
-                            + '{:2}'.format(date_tuple[2]) + ' '
-                            + '{:2}'.format(date_tuple[3]) + ':'
-                            + '{:2}'.format(date_tuple[4]) + ':'
-                            + '{:2}'.format(date_tuple[5]))
-    return date_string
+try:
+    os.mkdir('temporary_files')
+except FileExistsError:
+    shutil.rmtree('temporary_files')
+    os.mkdir('temporary_files')
+os.chdir('temporary_files')
+
+archive.extractall()
+
+def custom_split(input_list):
+    splitted = []
+    temp_str = ''
+    i = 0
+    while True:
+        if i >= len(input_list):
+            break
+        if input_list[i][0] == '\'':
+            cnt = 0
+            for j in range(i, len(input_list)):
+                if input_list[j][len(input_list[j]) - 1] == '\'':
+                    temp_str += input_list[j]
+                    splitted.append(temp_str[1:len(temp_str) - 1])
+                    cnt += 1
+                    break
+                temp_str += input_list[j] + ' '
+                cnt += 1
+            i += cnt
+        else:
+            splitted.append(input_list[i])
+        i += 1
+    return splitted
+
+# Exiting and Ctrl + C handling
+def exit(signal, frame):
+    os.chdir(vshell_root_dir)
+    try:
+        shutil.rmtree('temporary_files')
+    except Exception:
+        pass
+    print(colored('\nExiting VShell...', 'green'))
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, exit)
 
 def pwd():
-    print('/' + path.name)
+    current_dir = os.getcwd()
+    if user_os == 'Windows':
+        current_dir = current_dir.replace('\\', '/')
+    print(current_dir[len(vshell_root_dir + '/temporary_files'):] + '/')
     return
 
-def cd(input_list):
+def pwd_str():
+    current_dir = os.getcwd()
+    if user_os == 'Windows':
+        current_dir = current_dir.replace('\\', '/')
+    str = current_dir[len(vshell_root_dir + '/temporary_files'):] + '/'
+    return str
 
+def cd(input_list):
+    if len(input_list) == 1:
+        return
+    change_dir_list = input_list[1].split('/')
+    for i in range(len(change_dir_list)):
+        if change_dir_list[i] == '' and i == 0:
+            os.chdir(vshell_root_dir + '/temporary_files')
+            continue
+        elif change_dir_list[i] == '':
+            continue
+        elif change_dir_list[i] == '..' and pwd_str() == '/':
+            continue
+        try:
+            os.chdir(change_dir_list[i])
+        except NotADirectoryError:
+            print('cd: ' + input_list[1] + ': Not a directory')
+            break
+        except OSError:
+            print('cd: ' + input_list[1] + ': No such file or directory')
+            break
     return
 
 def cat(input_list):
@@ -30,34 +102,28 @@ def cat(input_list):
 
 def ls(input_list):
     if len(input_list) == 1:
-        for filename in archive.namelist():
-            temp_name = filename.split('/')
-            path_length = len(path.name.split('/'))
-            if len(path.name) == 0:
-                path_length -= 1
-            if temp_name[len(temp_name) - 1] == '':
-                if filename.startswith(path.name) and len(temp_name) == path_length + 2:
-                    print(colored(temp_name[len(temp_name) - 2], 'blue'))
+        for cur_dir in os.scandir():
+            cur_dir_name = cur_dir.name
+            if ' ' in cur_dir_name:
+                cur_dir_name = '\'{0}\''.format(cur_dir_name)
+            if cur_dir.is_dir():
+                print(colored(cur_dir_name, 'blue'))
             else:
-                if filename.startswith(path.name) and len(temp_name) == path_length + 1:
-                    print(temp_name[len(temp_name) - 1])
+                print(cur_dir_name)
     elif len(input_list) == 2:
         if input_list[1] == '-l':
-            namelist = archive.namelist()
-            infolist = archive.infolist()
-            for i in range(len(namelist)):
-                temp_name = namelist[i].split('/')
-                path_length = len(path.name.split('/'))
-                if len(path.name) == 0:
-                    path_length -= 1
-                if temp_name[len(temp_name) - 1] == '':
-                    if namelist[i].startswith(path.name) and len(temp_name) == path_length + 2:
-                        print('           ' + make_date_string(infolist[i].date_time) + ' ' + colored(temp_name[len(temp_name) - 2], 'blue'))
+            for cur_dir in os.scandir():
+                seconds_date = cur_dir.stat().st_mtime
+                date = datetime.fromtimestamp(seconds_date, tz=None)
+                cur_dir_name = cur_dir.name
+                if ' ' in cur_dir_name:
+                    cur_dir_name = '\'{0}\''.format(cur_dir_name)
+                if cur_dir.is_dir():
+                    print('           ' + date.strftime('%Y %b %d %H:%M') + ' ' + colored(cur_dir_name, 'blue'))
                 else:
-                    if namelist[i].startswith(path.name) and len(temp_name) == path_length + 1:
-                        print('{:<10}'.format(infolist[i].file_size) + ' ' + make_date_string(infolist[i].date_time) + ' ' + temp_name[len(temp_name) - 1])
+                    print('{:<10}'.format(cur_dir.stat().st_size) + ' ' + date.strftime('%Y %b %d %H:%M') + ' ' + cur_dir_name)
         else:
-            print('ls: invalid option -- \'' +  input_list[1] + '\'')
+            print('ls: invalid option -- \'' + input_list[1] + '\'')
     else:
         print('ls: too many arguments')
     return
@@ -65,12 +131,13 @@ def ls(input_list):
 input_string = ''
 
 while True:
-    print(colored('/' + path.name, 'blue') + '>', end='')
+    print(colored(pwd_str(), 'blue') + '>', end='')
     input_string = input()
     input_list = list(input_string.split())
+    input_list = custom_split(input_list)
     if len(input_list) > 0:
         if input_list[0] == 'exit':
-            print(colored('Exiting VShell...', 'green'))
+            exit(0, 0)
             break
         if input_list[0] == 'pwd':
             pwd()
